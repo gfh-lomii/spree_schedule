a7b9cff4d3be7e47a5cd8b1d47898538491e54f5
rmartinezapp <robert.martinez.app@gmail.com>
2023-05-16 04:34:18 -0400

    add stock_locations/schedules API

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Spree::Core::Engine.add_routes do
   namespace :admin, path: Spree.admin_path do
     resources :schedules
   end
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :stock_locations do
+        member do
+          get :schedules
+        end
+      end
+    end
+  end
 end
